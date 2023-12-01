8733a99d13242ae27cedd85d4722f0fd8e91326d
Md Rafiqul Islam <rafiqulislam.dev@gmail.com>
2023-12-01 23:07:48 +0600

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { Auth0Provider } from "@auth0/auth0-react"
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import Context from './Context/Context.jsx'
-import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Context from "./Context/Context.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
